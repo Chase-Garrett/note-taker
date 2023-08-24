eb9219e6df94fe3b5af924287c62b8a5f17e71ce
Chase <chasegarrett@tutanota.com>
2023-08-24 09:08:04 -0500

    added notes and 404 routes to server

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const app = express();
 // middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// use api route
+app.use("/api", api);
 
 // serve up static assets
 app.use(express.static("public"));
@@ -20,6 +22,16 @@ app.use(express.static("public"));
 // GET Route for homepage
 app.get("/", (req, res) =>
     res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+// GET Route for notes
+app.get("/notes", (req, res) =>
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+// GET wildcard route for 404 error
+app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname, "/public/404.html"))
 );
 
 // listener
